54b05dec5d0eb8cba9a65b27720afeb70e81fe38
Dave Vasilevsky <djvasi@gmail.com>
2025-01-21 17:48:24 -0500

    systemd-boot

--- BootLinux.ps1
+++ BootLinux.ps1
@@ -6,9 +6,12 @@ if (-not (Test-Path S:)) {
 # Strings I've seen:
 # Boot Microsoft EFI boot from EFI system partition
 # Boot boot\vmlinuz-5.10-x86_64 from root  
+# Boot EFI\systemd\systemd-bootx64.efi
 
 # Set to boot string + space + null byte. Or just a prefix
 $os = "Boot boot\vmlinuz-`0"
+#$os = "Boot EFI\systemd\systemd-bootx64.efi`0"
+
 # Write as UTF16-LE without BOM
 $bytes = [system.Text.Encoding]::Unicode.GetBytes($os)
 # Can't use Set-Content on arbitrary filesystem
